a80a56dd6ae9cc5a547ca09f22ae448c2edd238e
Lucas CLEMENTEI <clementeilucas@gmail.com>
2019-03-20 16:44:59 +0100

    Ajout cercle click et label

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,3 +1,54 @@
+var graph = new	Graph("Graph1",false);
+
+
+$(document).ready(function(){
+var elem = document.getElementById('canvas-graph'),
+    elemLeft = elem.offsetLeft,
+    elemTop = elem.offsetTop,
+    context = elem.getContext('2d'),
+    vertex = [];
+
+// Add event listener for `click` events.
+elem.addEventListener('click', function(event) {
+    var x = event.pageX - elemLeft,
+        y = event.pageY - elemTop;
+        console.log(x,y);
+        context.beginPath();
+		context.arc(x, y, 10, 0, 2 * Math.PI);
+		context.fillStyle = "black";
+		context.fill();
+		context.font = "10px Arial";
+		context.fillStyle = "red";
+		context.textAlign = 'center'; 
+		context.fillText("666", x, y);
+		context.stroke();
+    // Collision detection between clicked offset and elements.
+    graph.vertices.forEach(function(element) {
+        if (y > element.top && y < element.top + element.height 
+            && x > element.left && x < element.left + element.width) {
+            alert("Le noeud :"+element.id);
+        }
+    });
+    // Add element.
+	graph.addVertex(new Pos(x,y),'')
+}, false);
+
+
+
+
+// Render vertex.
+vertex.forEach(function(element) {
+	context.beginPath();
+    context.fillStyle = element.colour;
+    context.fillRect(element.left, element.top, element.width, element.height);
+    context.stroke();
+})
+
+})
+
+
+
+
 function addVertice() {
 
 }
